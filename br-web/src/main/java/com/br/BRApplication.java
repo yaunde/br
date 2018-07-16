659c7a4034b485e6a83d5d62da470dc04b919991
yaunde <953720113@qq.com>
2018-07-16 23:01:41 +0800

    hello程序

---
+++ br-web/src/main/java/com/br/BRApplication.java
@@ -0,0 +1,11 @@
+package com.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BRApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BRApplication.class, args);
+    }
+}
